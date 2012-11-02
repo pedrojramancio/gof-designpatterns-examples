858ef9d56abb5f150b8591783225026aaefe6ecd
pedrojramancio <pedrojramancio@f824ec29-6e55-51df-fc46-3dee90a1847a>
2012-11-02 20:45:22 +0000

    Design pattern Builder

---
+++ DesignPatterns/src/CIM/dp11/Builder/SoupBuffetBuilder.java
@@ -0,0 +1,38 @@
+package CIM.dp11.Builder;
+
+ abstract class SoupBuffetBuilder {
+    SoupBuffet soupBuffet;
+        
+    public SoupBuffet getSoupBuffet() {
+        return soupBuffet;
+    }
+    
+    public void buildSoupBuffet() {
+        soupBuffet = new SoupBuffet();
+    }
+    
+    public abstract void setSoupBuffetName();
+        
+    public void buildChickenSoup() {
+        soupBuffet.chickenSoup = new ChickenSoup();
+    }
+//	ensopado de marisco
+    public void buildClamChowder() {
+        soupBuffet.clamChowder = new ClamChowder();
+    }
+    public void buildFishChowder() {
+        soupBuffet.fishChowder = new FishChowder();
+    }
+    public void buildMinnestrone() {
+        soupBuffet.minnestrone = new Minnestrone();
+    }
+    public void buildPastaFazul() {
+        soupBuffet.pastaFazul = new PastaFazul();
+    }
+    public void buildTofuSoup() {
+        soupBuffet.tofuSoup = new TofuSoup();
+    }
+    public void buildVegetableSoup() {
+        soupBuffet.vegetableSoup = new VegetableSoup();
+    }
+}
