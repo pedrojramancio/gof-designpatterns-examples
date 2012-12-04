22d244bbab747f2618f575ad990e1837f3d4f8a2
pedrojramancio <pedrojramancio@f824ec29-6e55-51df-fc46-3dee90a1847a>
2012-12-04 23:29:36 +0000

    correção em getFrequentRenterPoints()

--- tag6/src/SIM/refactoring/Rental.java
+++ tag6/src/SIM/refactoring/Rental.java
@@ -44,9 +44,9 @@ public class Rental {
 		// add bonus for a two days new release rental
 		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
 				&& getDaysRented() > 1)
-			return 1;
+			return 2;
 		else
-			return 0;
+			return 1;
 	}
 
 }
