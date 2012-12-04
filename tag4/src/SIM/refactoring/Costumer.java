36a604d52746335c1781269e64db86aadbba2a03
pedrojramancio <pedrojramancio@f824ec29-6e55-51df-fc46-3dee90a1847a>
2012-12-04 01:30:03 +0000

    Extract Method

---
+++ tag4/src/SIM/refactoring/Costumer.java
@@ -0,0 +1,53 @@
+package SIM.refactoring;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Costumer {
+
+	private String _name;
+	private Vector _rentals = new Vector();
+
+	public Costumer(String name) {
+		_name = name;
+	}
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String Statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = (Rental) rentals.nextElement();
+
+			thisAmount = each.getCharge();
+			
+			// add frequenter renter points
+			frequentRenterPoints++;
+
+			// add bonus for a two days new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&& each.getDaysRented() > 1)
+				frequentRenterPoints++;
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount
+					+ "\n";
+			totalAmount += thisAmount;
+		}
+		// add footer lines
+		result += "Amount owed is " + totalAmount + "\n";
+		result += "You earned " + frequentRenterPoints
+				+ " frequent renter points";
+		return result;
+	} // end statement()
+
+}
