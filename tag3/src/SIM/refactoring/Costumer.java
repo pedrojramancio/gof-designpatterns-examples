8b651f29ca9b1af17e59020db1cd61463895d220
pedrojramancio <pedrojramancio@f824ec29-6e55-51df-fc46-3dee90a1847a>
2012-12-04 00:44:20 +0000

    Mudar nomes das variáveis

---
+++ tag3/src/SIM/refactoring/Costumer.java
@@ -0,0 +1,74 @@
+package SIM.refactoring;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Costumer {
+
+	private String _name;
+	private Vector _rentals = new Vector();
+
+	public Costumer(String name) {
+		_name = name;
+	}
+
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public String Statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration rentals = _rentals.elements();
+		String result = "Rental Record for " + getName() + "\n";
+		while (rentals.hasMoreElements()) {
+			double thisAmount = 0;
+			Rental each = (Rental) rentals.nextElement();
+
+			thisAmount = amountFor(each);
+			
+			// add frequenter renter points
+			frequentRenterPoints++;
+
+			// add bonus for a two days new release rental
+			if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE)
+					&& each.getDaysRented() > 1)
+				frequentRenterPoints++;
+			// show figures for this rental
+			result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount
+					+ "\n";
+			totalAmount += thisAmount;
+		}
+		// add footer lines
+		result += "Amount owed is " + totalAmount + "\n";
+		result += "You earned " + frequentRenterPoints
+				+ " frequent renter points";
+		return result;
+	} // end statement()
+
+	// Extract Method
+	private double amountFor(Rental aRental) {
+		int result = 0;
+		// determine amounts for each line
+		switch (aRental.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (aRental.getDaysRented() > 2)
+				result += (aRental.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			result += aRental.getDaysRented() * 3;
+			break;
+		case Movie.CHILDREN:
+			result += 1.5;
+			if (aRental.getDaysRented() > 3)
+				result += (aRental.getDaysRented() - 3) * 1.5;
+			break;
+		}		
+		return result;
+	}
+}
